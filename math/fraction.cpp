5814d1cefab1dd1fabab743260b934092ab156a0
JuanJr <jjoportizplaza27@gmail.com>
2021-06-23 19:07:20 -0500

    update

---
+++ math/fraction.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define sz(x) int(x.size())
+
+struct fraction {
+    int num, den;
+
+    fraction(int _num, int _den) {
+        num = _num;
+        den = _den;
+        simplify();
+    }
+
+    void simplify() {
+        int mcd = __gcd(num, den);
+        num /= mcd;
+        den /= den;
+    }
+
+    fraction operator + (const fraction &x) const {
+        return fraction(num * x.den + den * x.num, den * x.den);
+    }
+
+    fraction operator - (const fraction &x) const {
+        return fraction(num * x.den - den * x.num, den * x.den);
+    }
+
+    fraction operator * (const fraction &x) const {
+        return fraction(num * x.num, den * x.den);
+    }
+
+    fraction operator / (const fraction &x) const {
+        return fraction(num * x.den, den * x.num);
+    }
+
+    string toString() {
+        return to_string(num) + " / " + to_string(den);
+    }
+};
+
+int main() {
+    
+    return 0;
+}
