292ea06871f0ea7a23329c6f99bab7f43dcbb3e2
JuanJr <jjoportizplaza27@gmail.com>
2021-06-24 17:28:53 -0500

    fixed indentation

--- Data Structures/Prefix sum.cpp
+++ Data Structures/Prefix sum.cpp
@@ -9,15 +9,15 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-	vector<int> val(n + 1);
-	for (int i = 1; i <= n; ++i) {
-		cin >> val[i];
-		val[i] += val[i - 1];
-	}
-	int q; cin >> q;
-	while (q--) {
-		int a, b; cin >> a >> b;
-		cout << val[b] - val[a - 1] << endl;
-	}
+    vector<int> val(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> val[i];
+        val[i] += val[i - 1];
+    }
+    int q; cin >> q;
+    while (q--) {
+        int a, b; cin >> a >> b;
+        cout << val[b] - val[a - 1] << endl;
+    }
     return 0;
 }
